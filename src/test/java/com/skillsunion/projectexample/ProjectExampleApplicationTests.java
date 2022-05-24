5afebe07ffeed62a8dea71b246c086fbf9f69185
Edison Z <edisen.zhuang@gmail.com>
2022-05-24 10:48:29 +0800

    initial commit

---
+++ src/test/java/com/skillsunion/projectexample/ProjectExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillsunion.projectexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
