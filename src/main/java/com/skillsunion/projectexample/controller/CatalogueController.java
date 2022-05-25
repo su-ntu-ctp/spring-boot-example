9fd839fe57b819c16b43b5e6338bc0c92fc3bb4e
Edison Z <edisen.zhuang@gmail.com>
2022-05-25 10:51:05 +0800

    get one. return error status.

--- src/main/java/com/skillsunion/projectexample/controller/CatalogueController.java
+++ src/main/java/com/skillsunion/projectexample/controller/CatalogueController.java
@@ -1,6 +1,7 @@
 package com.skillsunion.projectexample.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import com.skillsunion.projectexample.entity.Catalogue;
 import com.skillsunion.projectexample.service.CatalogueService;
@@ -9,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/catalogues")
@@ -23,8 +26,20 @@ public class CatalogueController {
     
     @GetMapping
     public @ResponseBody List<Catalogue> list(){
-
         return service.list();
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody Catalogue get(@PathVariable int id){
+        try{
+            return service.get(id);
+        }catch(NoSuchElementException e){
+            // This error is thrown by service when element with the given id is not found.
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }catch(Exception e){
+            // When any other errors thrown...
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping
--- src/main/java/com/skillsunion/projectexample/service/CatalogueService.java
+++ src/main/java/com/skillsunion/projectexample/service/CatalogueService.java
@@ -1,6 +1,7 @@
 package com.skillsunion.projectexample.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.skillsunion.projectexample.entity.Catalogue;
 import com.skillsunion.projectexample.repository.CatalogueRepository;
@@ -11,14 +12,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class CatalogueService {
 
-    @Autowired CatalogueRepository catalogueRepo;
+    @Autowired CatalogueRepository repo;
 
     public List<Catalogue> list(){
-        return (List<Catalogue>)catalogueRepo.findAll();
+        return (List<Catalogue>)repo.findAll();
     }
 
     public Catalogue create(Catalogue catalogue){
-        return catalogueRepo.save(catalogue);
+        return repo.save(catalogue);
+    }
+
+    public Catalogue get(int id){
+        Optional<Catalogue> found = repo.findById(id);
+        return found.get();
     }
     
 }
